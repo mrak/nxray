51a2248a16df41e8e765621e0a4ad3667da0d433
Eric Mrak <enmrak@gmail.com>
2024-07-18 15:49:44 -0700

    port tests

--- src/bin/nx/args.rs
+++ src/bin/nx/args.rs
@@ -2,6 +2,7 @@ use std::str::FromStr;
 use pest_derive::Parser;
 use pest::error::Error;
 use pest::Parser;
+use pest::iterators::Pair;
 
 #[derive(Parser)]
 #[grammar = "bin/nx/args.pest"]
@@ -21,10 +22,11 @@ pub enum PortOption {
 }
 
 #[derive(PartialEq,Debug)]
-pub enum AddressType {
-    MACAddress(u64), // 48 bit, really
-    IPv4Address(IPv4Addr, IPv4Mask, PortOption),
-    IPv6Address(IPv6Addr, IPv6Mask, PortOption),
+pub enum Address {
+    MAC(u64), // 48 bit, really
+    IPv4(IPv4Addr, IPv4Mask, PortOption),
+    IPv6(IPv6Addr, IPv6Mask, PortOption),
+    PortOnly(PortOption),
 }
 
 #[derive(PartialEq,Debug)]
@@ -60,31 +62,60 @@ pub enum Argument<'a> {
     Pcap,
     Interface(&'a str),
     ProtocolOpt(Protocol, Option<bool>),
-    AddressFilter(Option<PacketDirection>, AddressType),
-    SrcDstFilter(AddressType, AddressType), // src, dest
-    BetweenFilter(AddressType, AddressType), // src or dest, src or dest
+    AddressFilter(PacketDirection, Address),
+    SrcDstFilter(Address, Address), // src, dest
+    BetweenFilter(Address, Address), // src or dest, src or dest
 }
 
 pub fn parse_arg(argstr: &str) -> Result<Argument, Error<Rule>> {
+    fn parse_protocol_opt(arg: Pair<Rule>) -> Argument {
+        let option = match arg.as_str().chars().nth(0) {
+            Some('-') => Some(false),
+            Some('+') => Some(true),
+            _ => None,
+        };
+        let protocol = match option {
+            Some(_) => Protocol::from_str(arg.as_str().get(1..).unwrap()).unwrap(),
+            None => Protocol::from_str(arg.as_str()).unwrap(),
+        };
+        Argument::ProtocolOpt(protocol, option)
+    }
+
+    fn parse_port(arg: Pair<Rule>) -> u16 {
+        arg.as_str().parse::<u16>().unwrap()
+    }
+
+    fn parse_address(arg: Pair<Rule>) -> Argument {
+        let a = arg.into_inner().next().unwrap();
+        match a.as_rule() {
+            Rule::port_opt => {
+                println!("here:{a:?}");
+                let opt = a.into_inner().next().unwrap();
+                let port_opt = match opt.as_rule() {
+                    Rule::port => PortOption::Specific(opt.as_str().parse::<u16>().unwrap()),
+                    Rule::port_list => PortOption::List(opt.into_inner().map(parse_port).collect()),
+                    _ => todo!()
+                };
+                Argument::AddressFilter(PacketDirection::Either, Address::PortOnly(port_opt))
+            },
+            Rule::ipv4_address => todo!(),
+            Rule::ipv6_address => todo!(),
+            Rule::mac_address => todo!(),
+            _ => unreachable!()
+        }
+    }
+
+    //let one = ARGParser::parse(Rule::argument, argstr)?.next();
+    //let mut two = one.clone().unwrap().into_inner();
+    //let three = two.next().unwrap();
+    //println!("\none:{one:?}\ntwo:{two:?}\nthree:{three:?}");
     let arg = ARGParser::parse(Rule::argument, argstr)?.next().unwrap().into_inner().next().unwrap();
 
     return Ok(match arg.as_rule() {
         Rule::pcap => Argument::Pcap,
-        Rule::protocol_opt => {
-            let option = match arg.as_str().chars().nth(0) {
-                Some('-') => Some(false),
-                Some('+') => Some(true),
-                _ => None,
-            };
-            let protocol = match option {
-                Some(_) => Protocol::from_str(arg.as_str().get(1..).unwrap()).unwrap(),
-                None => Protocol::from_str(arg.as_str()).unwrap(),
-            };
-            Argument::ProtocolOpt(protocol, option)
-        },
-        Rule::interface => {
-            Argument::Interface(arg.as_str())
-        }
+        Rule::protocol_opt => parse_protocol_opt(arg),
+        Rule::address => parse_address(arg),
+        Rule::interface => Argument::Interface(arg.as_str()),
         _ => todo!(),
     })
 }
@@ -103,17 +134,41 @@ mod tests {
     fn protocol() {
         let result = parse_arg("tcp");
         assert_eq!(result.unwrap(), Argument::ProtocolOpt(Protocol::TCP, None));
+        let result = parse_arg("-tcp");
+        assert_eq!(result.unwrap(), Argument::ProtocolOpt(Protocol::TCP, Some(false)));
         let result = parse_arg("+tcp");
         assert_eq!(result.unwrap(), Argument::ProtocolOpt(Protocol::TCP, Some(true)));
+        let result = parse_arg("udp");
+        assert_eq!(result.unwrap(), Argument::ProtocolOpt(Protocol::UDP, None));
+        let result = parse_arg("-udp");
+        assert_eq!(result.unwrap(), Argument::ProtocolOpt(Protocol::UDP, Some(false)));
+        let result = parse_arg("+udp");
+        assert_eq!(result.unwrap(), Argument::ProtocolOpt(Protocol::UDP, Some(true)));
+        let result = parse_arg("icmp");
+        assert_eq!(result.unwrap(), Argument::ProtocolOpt(Protocol::ICMP, None));
         let result = parse_arg("-icmp");
         assert_eq!(result.unwrap(), Argument::ProtocolOpt(Protocol::ICMP, Some(false)));
+        let result = parse_arg("+icmp");
+        assert_eq!(result.unwrap(), Argument::ProtocolOpt(Protocol::ICMP, Some(true)));
         let result = parse_arg("ethernet");
         assert_eq!(result.unwrap(), Argument::ProtocolOpt(Protocol::Ethernet, None));
+    }
+
+    #[test]
+    fn port() {
+        let result = parse_arg(":8080");
+        assert_eq!(result.unwrap(), Argument::AddressFilter(PacketDirection::Either, Address::PortOnly(PortOption::Specific(8080))));
+        let result = parse_arg(":80,443");
+        assert_eq!(result.unwrap(), Argument::AddressFilter(PacketDirection::Either, Address::PortOnly(PortOption::List(vec!(80,443)))));
+        let result = parse_arg(":80,443,8080,8443");
+        assert_eq!(result.unwrap(), Argument::AddressFilter(PacketDirection::Either, Address::PortOnly(PortOption::List(vec!(80,443,8080,8443)))));
     }
 
     #[test]
     fn interface() {
         let result = parse_arg("eth0");
         assert_eq!(result.unwrap(), Argument::Interface("eth0"));
+        let result = parse_arg("docker0");
+        assert_eq!(result.unwrap(), Argument::Interface("docker0"));
     }
 }
